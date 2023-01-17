92a623fe94c90f2e4db0a1761f29bc464c15032e
jackiechen2333 <65798428+jackiechen2333@users.noreply.github.com>
2023-01-17 20:52:00 +0800

    pull commit

--- src/com/cx/java/customer.java
+++ src/com/cx/java/customer.java
@@ -13,6 +13,7 @@ public class customer {
         System.out.println("master test!");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
