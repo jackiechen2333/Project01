32f8811483616863df7681c9fa80927c6589f016
cx <1670450775@qq.com>
2023-01-17 21:19:18 +0800

    gitee commit

--- src/com/cx/java/customer.java
+++ src/com/cx/java/customer.java
@@ -14,6 +14,7 @@ public class customer {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
     }
 
 }
