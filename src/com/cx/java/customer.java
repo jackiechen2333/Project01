d7c64eb260c0f30d38da4b64fe788486757eb2a5
cx <1670450775@qq.com>
2023-01-17 20:19:52 +0800

    first commit

---
+++ src/com/cx/java/customer.java
@@ -0,0 +1,13 @@
+package com.cx.java;
+
+/**
+ * @author cx
+ * @date 2023/01/17/15:04
+ */
+public class customer {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+
+    }
+
+}
